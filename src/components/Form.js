f4dd1f4d7f3c80181c1e183687937478f2a8adec
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-27 17:44:22 -0400

    Initial states set

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import bgimage from "../Assets/Pizza.jpg";
 import {
   Container,
@@ -13,8 +13,42 @@ import {
   Label,
   Input
 } from "reactstrap";
+import axios from "axios";
+import * as yup from "yup";
+
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(2)
+    .required("Must have a name for the order")
+});
 
 export default function MyForm() {
+  // state for whether button should be disabled or not
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  // managing state for our form inputs
+  const [formState, setFormState] = useState({
+    name: "",
+    pizzaSize: "Large",
+    sauce: "Normal",
+    crust: "",
+    pepperoni: "",
+    sausage: "",
+    canadianBacon: "",
+    italianSausage: "",
+    grilledChicken: "",
+    onions: "",
+    greenPepper: "",
+    dicedTomatos: "",
+    blackOlives: "",
+    roastedGarlic: "",
+    artichokeHearts: "",
+    threeCheese: "",
+    pineapple: "",
+    extraCheese: "",
+    textArea: ""
+  });
   return (
     <>
       <Jumbotron
@@ -41,7 +75,7 @@ export default function MyForm() {
                         id='name'
                         type='text'
                         name='name'
-                        value=''
+                        value={formState.name}
                         onChange=''
                       />
                     </FormGroup>
@@ -56,7 +90,7 @@ export default function MyForm() {
                         id='pizzaSize'
                         type='select'
                         name='pizzaSize'
-                        value='Large'
+                        value={formState.pizzaSize}
                         onChange=''
                       >
                         <option value='Small'>Small</option>
@@ -72,7 +106,7 @@ export default function MyForm() {
                         id='sauce'
                         type='select'
                         name='sauce'
-                        value='Normal'
+                        value={formState.sauce}
                         onChange=''
                       >
                         <option value='Light'>Light</option>
@@ -86,12 +120,7 @@ export default function MyForm() {
                       <legend>Choose A Crust</legend>
                       <FormGroup check>
                         <Label htmlFor='original' />
-                        <Input
-                          id='original'
-                          type='radio'
-                          name='crust'
-                          checked='true'
-                        />{" "}
+                        <Input id='original' type='radio' name='crust' />{" "}
                         Original Crust
                       </FormGroup>
                       <FormGroup check>
@@ -146,12 +175,18 @@ export default function MyForm() {
                           id='pepperoni'
                           type='checkbox'
                           name='pepperoni'
+                          checked={formState.pepperoni}
                         />
                         Pepperoni
                       </FormGroup>
                       <FormGroup check>
                         <Label htmlFor='sausage' />
-                        <Input id='sausage' type='checkbox' name='sausage' />
+                        <Input
+                          id='sausage'
+                          type='checkbox'
+                          name='sausage'
+                          checked={formState.sausage}
+                        />
                         Sausage
                       </FormGroup>
                       <FormGroup check>
@@ -160,6 +195,7 @@ export default function MyForm() {
                           id='canadianBacon'
                           type='checkbox'
                           name='canadianBacon'
+                          checked={formState.canadianBacon}
                         />
                         Canadian Bacon
                       </FormGroup>
@@ -169,6 +205,7 @@ export default function MyForm() {
                           id='italianSausage'
                           type='checkbox'
                           name='italianSausage'
+                          checked={formState.italianSausage}
                         />
                         Spicey Italian Sausage
                       </FormGroup>
@@ -178,12 +215,18 @@ export default function MyForm() {
                           id='grilledChicken'
                           type='checkbox'
                           name='grilledChicken'
+                          checked={formState.grilledChicken}
                         />
                         Grilled Chicken
                       </FormGroup>
                       <FormGroup check>
                         <Label htmlFor='onions' />
-                        <Input id='onions' type='checkbox' name='onions' />
+                        <Input
+                          id='onions'
+                          type='checkbox'
+                          name='onions'
+                          checked={formState.onions}
+                        />
                         Onions
                       </FormGroup>
                       <FormGroup check>
@@ -192,6 +235,7 @@ export default function MyForm() {
                           id='greenPepper'
                           type='checkbox'
                           name='greenPepper'
+                          checked={formState.greenPepper}
                         />
                         Green Pepper
                       </FormGroup>
@@ -200,60 +244,89 @@ export default function MyForm() {
                   <Col md={{ size: 4, offset: 2 }}>
                     <FormGroup>
                       <FormGroup check>
-                        <Label htmlFor='pepperoni' />
+                        <Label htmlFor='dicedTomatos' />
                         <Input
-                          id='pepperoni'
+                          id='dicedTomatos'
                           type='checkbox'
-                          name='pepperoni'
+                          name='dicedTomatos'
+                          checked={formState.dicedTomatos}
                         />
-                        Pepperoni
+                        Diced Tomatos
                       </FormGroup>
                       <FormGroup check>
-                        <Label htmlFor='sausage' />
-                        <Input id='sausage' type='checkbox' name='sausage' />
-                        Sausage
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label htmlFor='canadianBacon' />
+                        <Label htmlFor='blackOlives' />
                         <Input
-                          id='canadianBacon'
+                          id='blackOlives'
                           type='checkbox'
-                          name='canadianBacon'
+                          name='blackOlives'
+                          checked={formState.blackOlives}
                         />
-                        Canadian Bacon
+                        Black Olives
                       </FormGroup>
                       <FormGroup check>
-                        <Label htmlFor='italianSausage' />
+                        <Label htmlFor='roastedGarlic' />
                         <Input
-                          id='italianSausage'
+                          id='roastedGarlic'
                           type='checkbox'
-                          name='italianSausage'
+                          name='roastedGarlic'
+                          checked={formState.roastedGarlic}
                         />
-                        Spicey Italian Sausage
+                        Roasted Garlic
                       </FormGroup>
                       <FormGroup check>
-                        <Label htmlFor='grilledChicken' />
+                        <Label htmlFor='artichokeHearts' />
                         <Input
-                          id='grilledChicken'
+                          id='artichokeHearts'
                           type='checkbox'
-                          name='grilledChicken'
+                          name='artichokeHearts'
+                          checked={formState.artichokeHearts}
                         />
-                        Grilled Chicken
+                        Artichoke Hearts
                       </FormGroup>
                       <FormGroup check>
-                        <Label htmlFor='onions' />
-                        <Input id='onions' type='checkbox' name='onions' />
-                        Onions
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label htmlFor='greenPepper' />
+                        <Label htmlFor='threeCheese' />
                         <Input
-                          id='greenPepper'
+                          id='threeCheese'
                           type='checkbox'
-                          name='greenPepper'
+                          name='threeCheese'
+                          checked={formState.threeCheese}
                         />
-                        Green Pepper
+                        Three Cheese
                       </FormGroup>
+                      <FormGroup check>
+                        <Label htmlFor='pineapple' />
+                        <Input
+                          id='pineapple'
+                          type='checkbox'
+                          name='pineapple'
+                          checked={formState.pineapple}
+                        />
+                        Pineapple
+                      </FormGroup>
+                      <FormGroup check>
+                        <Label htmlFor='extraCheese' />
+                        <Input
+                          id='extraCheese'
+                          type='checkbox'
+                          name='extraCheese'
+                          checked={formState.extraCheese}
+                        />
+                        Extra Cheese
+                      </FormGroup>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row form>
+                  <Col md={12}>
+                    <FormGroup>
+                      <Label htmlFor='textArea' />
+                      <legend>Special Instructions</legend>
+                      <Input
+                        id='textArea'
+                        type='textarea'
+                        name='textArea'
+                        value={formState.textArea}
+                      />
                     </FormGroup>
                   </Col>
                 </Row>
@@ -262,7 +335,9 @@ export default function MyForm() {
                     sm={{ size: "auto", offset: 5 }}
                     md={{ size: "auto", offset: 5 }}
                   >
-                    <Button color='primary'>Place Order</Button>
+                    <Button color='primary' disabled={buttonDisabled}>
+                      Place Order
+                    </Button>
                   </Col>
                 </Row>
               </Form>
